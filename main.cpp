7cbd0dd028b5ca5ddf2751ca214ca436a72e177f
白澪 · 卡丝蕾特 <mio@chyan.moe>
2024-08-22 13:03:04 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -47,15 +47,20 @@ double dot(int hash, double x, double y)
 {
     switch (hash & 3)
     {
-        case 0: return x + y;
-        case 1: return -x + y;
-        case 2: return x - y;
-        case 3: return -x - y;
+        case 0:
+            return x + y;
+        case 1:
+            return -x + y;
+        case 2:
+            return x - y;
+        case 3:
+            return -x - y;
     }
     std::exit(1);
 }
 
-double berlin2d(double x, double y) {
+double berlin2d(double x, double y)
+{
     int xn = fastFloor(x);
     int yn = fastFloor(y);
     double xf = x - xn;
@@ -65,11 +70,11 @@ double berlin2d(double x, double y) {
     yn = yn % 255;
     int aa = p[p[xn] + yn];
     int ab = p[p[xn + 1] + yn];
-    int ba = p[p[xn] + yn +1];
-    int bb = p[p[xn +1] + yn +1];
-    double v1 = lerp(fade(xf), dot(aa , xf , yf) , dot(ab , xf -1 , yf));
-    double v2 = lerp(fade(xf), dot(ba , xf , yf -1) , dot(bb , xf -1 , yf -1));
-    double v3 = lerp(fade(yf), v1,v2);
+    int ba = p[p[xn] + yn + 1];
+    int bb = p[p[xn + 1] + yn + 1];
+    double v1 = lerp(fade(xf), dot(aa, xf, yf), dot(ab, xf - 1, yf));
+    double v2 = lerp(fade(xf), dot(ba, xf, yf - 1), dot(bb, xf - 1, yf - 1));
+    double v3 = lerp(fade(yf), v1, v2);
     return v3;
 }
 
@@ -77,7 +82,10 @@ double berlin2d(double x, double y) {
 int main()
 {
 
-    double seed, ox, oy, line;
+    for (;;)
+    {
+
+        double seed, ox, oy, line;
 //    std::cout << "input seed ↓" << std::endl;
 //    if (!(std::cin >> seed)) {
 //        std::cerr << "Error reading seed." << std::endl;
@@ -92,41 +100,53 @@ int main()
 //        p[i] = dist(gen) & 1; // 随机生成 0 或 1
 //    }
 
-    std::cout << "input start point ↓ x (space) y" << std::endl;
-    if (!(std::cin >> ox >> oy)) {
-        std::cerr << "Error reading start point." << std::endl;
-        return 1;
-    }
-    std::cout << "input start side length ↓" << std::endl;
-    if (!(std::cin >> line)) {
-        std::cerr << "Error reading side length." << std::endl;
-        return 1;
-    }
-
-    for (double i = oy ; i < oy + line ; i+=0.1) {
-        for (double j = ox ; j <= ox + line ; j+=0.1) {
-            double result = berlin2d(i+100, j+100);
-            if (result  >= 0.75) {
-                std::cout << "\033[47m" << "  " << "\033[0m";
-            }else if (result  >= 0.5) {
-                std::cout << "\033[46m" << "  " << "\033[0m";
-            }else if (result  >= 0.25) {
-                std::cout << "\033[45m" << "  " << "\033[0m";
-            }else if (result  >= 0) {
-                std::cout << "\033[44m" << "  " << "\033[0m";
-            }else if (result  >= -0.2) {
-                std::cout << "\033[43m" << "  " << "\033[0m";
-            }else if (result  >= -0.4) {
-                std::cout << "\033[42m" << "  " << "\033[0m";
-            }else if (result  >= -0.6) {
-                std::cout << "\033[41m" << "  " << "\033[0m";
-            }else {
-                std::cout << "\033[40m" << "  " << "\033[0m";
-            }
-
+        std::cout << "input start point ↓ x (space) y" << std::endl;
+        if (!(std::cin >> ox >> oy))
+        {
+            std::cerr << "Error reading start point." << std::endl;
+            return 1;
         }
-        std::cout << std::endl;
-    }
+        std::cout << "input start side length ↓" << std::endl;
+        if (!(std::cin >> line))
+        {
+            std::cerr << "Error reading side length." << std::endl;
+            return 1;
+        }
 
-    if (std::cin >> line) {}
+        for (double i = oy; i < oy + line; i += 0.1)
+        {
+            for (double j = ox; j <= ox + line; j += 0.1)
+            {
+                double result = berlin2d(i + 100, j + 100);
+                if (result >= 0.75)
+                {
+                    std::cout << "\033[47m" << "  " << "\033[0m";
+                } else if (result >= 0.5)
+                {
+                    std::cout << "\033[46m" << "  " << "\033[0m";
+                } else if (result >= 0.25)
+                {
+                    std::cout << "\033[45m" << "  " << "\033[0m";
+                } else if (result >= 0)
+                {
+                    std::cout << "\033[44m" << "  " << "\033[0m";
+                } else if (result >= -0.2)
+                {
+                    std::cout << "\033[43m" << "  " << "\033[0m";
+                } else if (result >= -0.4)
+                {
+                    std::cout << "\033[42m" << "  " << "\033[0m";
+                } else if (result >= -0.6)
+                {
+                    std::cout << "\033[41m" << "  " << "\033[0m";
+                } else
+                {
+                    std::cout << "\033[40m" << "  " << "\033[0m";
+                }
+
+            }
+            std::cout << std::endl;
+        }
+
+    }
 }
